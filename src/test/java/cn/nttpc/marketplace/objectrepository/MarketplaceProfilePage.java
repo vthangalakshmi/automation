b3dc7067cfd311ec751110b0adabfb7577bc4fe9
Thangalakshmi <thangalakshmi@computenext.com>
2019-06-18 10:06:52 +0530

    Reverting the unwanted files

--- src/test/java/cn/nttpc/marketplace/objectrepository/MarketplaceProfilePage.java
+++
@@ -1,8 +0,0 @@
-package cn.nttpc.marketplace.objectrepository;
-
-import org.openqa.selenium.By;
-
-public class MarketplaceProfilePage {
-	public static By linkChangePassword=By.xpath("//*[@id='sidebar']/div/div/ul/li[2]/a");
-
-}
